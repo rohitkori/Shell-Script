a0e8975d4491d00297eb95ffa19e37901e74a49e
Rohit <kori.1@iitj.ac.in>
2023-12-17 01:17:21 +0530

    creating a shell script for installing nginx, docker, docker-compose and certbot in ubuntu

---
+++ deploySetUp.sh
@@ -0,0 +1,26 @@
+!/usr/bin/bash
+
+# install nginx
+sudo apt-get update
+sudo apt-get install nginx
+sudo service nginx status
+
+# install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+# install certbot
+sudo apt update
+sudo apt install snapd
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
