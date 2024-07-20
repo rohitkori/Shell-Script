5e7180175e78d84e019898dc54511f4f98064396
Rohit Kumar Kori <kori.1@iitj.ac.in>
2024-07-21 00:40:05 +0530

    Updating the Docker installation part

--- deploySetUp.sh
+++ deploySetUp.sh
@@ -3,16 +3,11 @@
 # install nginx
 sudo apt-get update
 echo "Y" | sudo apt-get install nginx
-#echo "q" | sudo service nginx status
 
 # install docker
-sudo apt update
-echo "Y" | sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "yes" | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-echo "Y" | sudo apt install docker-ce
-#echo "q" | sudo systemctl status docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
 
 # Execute the docker commands without sudo
 sudo usermod -aG docker ${USER} # Note: have to login again, then only it will work
